793b7434269c735e7bff7522649b1857ea4a4d09
mc_fdc <Masato.04.11.2007@gmail.com>
2023-07-09 22:24:00 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+from discord.ext import commands
+import discord
+
+
+class MyBot(commands.Bot):
+
+    async def setup_hook(self) -> None:
+        await self.load_extension("jishaku")
+        await self.load_extension("cogs.nsfw")
+
+    async def on_ready(self) -> None:
+        print("Ready")
+
+
+bot = MyBot(command_prefix="t2!", intents=discord.Intents.all())
+
+
+if __name__ == "__main__":
+    from os import getenv
+    bot.run(get_env("DISCORD_TOKEN"))
